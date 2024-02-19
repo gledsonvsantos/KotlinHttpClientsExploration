19bd1ac1000e2bfe6c83ccb9c269be28f52506f0
Gledson Santos <gledson.doidao@gmail.com>
2024-02-19 09:51:17 -0300

    add example proxy

--- src/main/kotlin/com/example/DemoController.kt
+++ src/main/kotlin/com/example/DemoController.kt
@@ -14,6 +14,8 @@ import jakarta.inject.Inject
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Proxy
 import java.net.URL
 
 
@@ -38,6 +40,10 @@ class DemoController(
     fun hello(): DogResponse? {
         // Request de forma imperativa
 //        val response = requestOkHttp()
+
+        // Request utilizando proxy
+//        val response = requestOkHttpProxy()
+
         // Request utilizando a biblioteca Micronaut
         val response = requestHttpClient()
         return response
@@ -63,6 +69,39 @@ class DemoController(
     private fun requestOkHttp(): DogResponse? {
         // Cria uma instância do cliente OkHttp
         val client = OkHttpClient()
+
+        // Cria uma requisição GET
+        val request = Request.Builder()
+            .url("https://dog.ceo/api/breeds/image/random")
+            .build()
+
+        // Executa a requisição e lida com a resposta
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Erro inesperado: $response")
+
+            // Pega o corpo da resposta
+            val body = response.body?.string()
+
+            // Converte o corpo da resposta em um objeto (serialização)
+            val dogResponse = gson.fromJson(body, DogResponse::class.java)
+
+            return dogResponse
+        }
+    }
+
+    private fun requestOkHttpProxy(): DogResponse? {
+        // Configura o endereço e a porta do proxy
+        val proxyAddress = InetSocketAddress("127.0.0.1", 3128)
+
+        //23:59:11.985 [io-executor-thread-1] ERROR i.m.http.server.RouteExecutor - Unexpected error occurred: Failed to connect to /127.0.0.1:1111
+
+        // Cria uma instância de Proxy
+        val proxy = Proxy(Proxy.Type.HTTP, proxyAddress)
+
+        // Cria uma instância do cliente OkHttp
+        val client = OkHttpClient.Builder()
+            .proxy(proxy)
+            .build()
 
         // Cria uma requisição GET
         val request = Request.Builder()
